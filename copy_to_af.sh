7ad5fc7f63f730940041bf384a52cd6648069d01
Evan Mallory <evanmall@andrew.cmu.edu>
2020-01-21 14:14:45 -0500

    Update copy_to_af.sh

--- copy_to_af.sh
+++ copy_to_af.sh
@@ -1,17 +1,18 @@
 set -xe
 
 DEST=../cs1-v2/frontend/src/brython
+LIB_DEST="$DEST/../../public/"
 
 (cd scripts && pipenv run python make_dist.py)
 
 rm -rf "$DEST"
 mkdir "$DEST"
 cp -r www/src/brython.js www/src/brython_stdlib.js setup "$DEST"
-cp -r www/src/builtins_docstrings.js www/src/libs www/src/Lib "$DEST/../../public/"
+cp -r www/src/builtins_docstrings.js www/src/libs www/src/Lib "$LIB_DEST"
 
 git log -n 1 --pretty=format:"%H" > $DEST/git-sha.txt
 
 # Remove files that we don't need
-rm -rf "$DEST/setup/data" "$DEST/Lib/test" "$DEST/setup/changelog.txt" \
-  "$DEST/Lib/.bundle-ignore" "$DEST/Lib/email/architecture.rst" \
-  "$DEST/Lib/importlib"
+rm -rf "$DEST/setup/data" "$LIB_DEST/Lib/test" "$DEST/setup/changelog.txt" "$DEST/setup/README.rst" \
+  "$LIB_DEST/Lib/.bundle-ignore" "$LIB_DEST/Lib/email/architecture.rst" \
+  "$LIB_DEST/Lib/importlib"
