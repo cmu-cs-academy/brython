13d56f0731a6d1eddc94e53ebcb6bd1b07d2fb2d
Austin Schick <aschick@andrew.cmu.edu>
2021-08-06 13:03:03 -0400

    Re-add cmu_graphics_bry, remove Foobar

--- setup/brython/__main__.py
+++ setup/brython/__main__.py
@@ -126,7 +126,7 @@ def main():
         finder = list_modules.ModulesFinder(stdlib=stdlib, user_modules=user_modules)
         # finder.inspect()
         finder.modules = {
-            'cs_academy_io', 'cmu_graphics', 'sys', 'traceback', 'hashlib', 'random', 'math', 'linecache',
+            'cs_academy_io', 'cmu_graphics', 'cmu_graphics_bry', 'sys', 'traceback', 'hashlib', 'random', 'math', 'linecache',
         }
         path = os.path.join(stdlib_dir, "brython_modules.js")
         finder.make_brython_modules(path)
