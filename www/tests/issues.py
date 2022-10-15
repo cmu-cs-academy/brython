d87389174f9845e9c4d234eba85e9cf724c5f7ab
PierreQuentel <pierre.quentel@gmail.com>
2022-10-15 16:07:01 +0200

    Fixes #2077 : JS exception when odd Unicode character is used as a function argument

--- www/tests/issues.py
+++ www/tests/issues.py
@@ -3145,6 +3145,9 @@ assert_raises(NameError, exec, src)
 
 assert_raises(NameError, exec, 'bar: Bar = 42')
 
+# issue 2077
+assert_raises(SyntaxError, exec, "f(🠞)",
+    msg="invalid character '🠞' (U+1F81E)")
 
 # ==========================================
 # Finally, report that all tests have passed
